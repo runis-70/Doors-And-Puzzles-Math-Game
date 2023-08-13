2ce526a8e218b6b6aa76d4688d6e70448970720b
nedoprogrammers <141520952+NEDOprogrammers@users.noreply.github.com>
2023-08-13 19:14:52 +0200

    Delete Scripts directory

--- Scripts/Dialog/DialogueScript.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DialogueScript : MonoBehaviour
-{
-    public List<DialogPoint> dialogPoints;
-}
--- Scripts/Dialog/PartnerDialog.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PartnerDialog : MonoBehaviour
-{
-    public string Name;
-    public Sprite Head;
-}
