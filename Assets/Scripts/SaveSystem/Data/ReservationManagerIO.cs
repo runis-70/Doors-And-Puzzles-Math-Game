8f1f94cc6deff9d0aa712389a211355c52ed78da
Денис <94766316+DSD-Games@users.noreply.github.com>
2023-08-23 20:55:21 +0700

    0.44
    - сделано начало новой игры, продолжение старой.

--- Assets/Scripts/SaveSystem/Data/ReservationManagerIO.cs
+++ Assets/Scripts/SaveSystem/Data/ReservationManagerIO.cs
@@ -2,39 +2,44 @@ using System;
 
 public class ReservationManagerIO : DataStream
 {
-    private string _pathSaveFile;
+    private string _pathReservationFile;
 
     public ReservationManagerIO(string pathSaveFile)
     {
-        _pathSaveFile = pathSaveFile;
+        _pathReservationFile = pathSaveFile;
     }
 
     public void CreateReservationPlayerData(PlayerData playerData)
     {
         if (playerData != null)
-            base.Serialize(_pathSaveFile, playerData);
+            base.Serialize(_pathReservationFile, playerData);
         else
             throw new Exception("Сохранения пусты");
     }
     public PlayerData LoadReservationPlayerData()
     {
-        return base.Deserialize<PlayerData>(_pathSaveFile);
+        return base.Deserialize<PlayerData>(_pathReservationFile);
     }
 
     public void CreateReservationApplicationData(ApplicationData applicationData)
     {
         if (applicationData != null)
-            base.Serialize(_pathSaveFile, applicationData);
+            base.Serialize(_pathReservationFile, applicationData);
         else
             throw new Exception("Сохранения пусты");
     }
     public ApplicationData LoadReservationApplicationData()
     {
-        return base.Deserialize<ApplicationData>(_pathSaveFile);
+        return base.Deserialize<ApplicationData>(_pathReservationFile);
     }
 
     public void Delete()
     {
-        base.Delete(_pathSaveFile);
+        base.Delete(_pathReservationFile);
+    }
+
+    public void DeleteAll()
+    {
+        base.DeleteAll(_pathReservationFile);
     }
 }
