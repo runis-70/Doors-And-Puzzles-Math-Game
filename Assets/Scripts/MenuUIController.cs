b3cbef1d7d0435d033570934563bbf074b46d5c1
Денис <94766316+DSD-Games@users.noreply.github.com>
2023-08-23 20:55:21 +0700

    0.44
    - сделано начало новой игры, продолжение старой.

--- Assets/Scripts/MenuUIController.cs
+++ Assets/Scripts/MenuUIController.cs
@@ -2,13 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuUIController : MonoBehaviour
 {
+    [SerializeField] private Button buttonBegin;
+    [SerializeField] private Button buttonContinue;
+    [SerializeField] private Button buttonNewGame;
     public Fade fade;
+
+    private ReservationManagerIO saveManagerIO;
+    private string pathToApplicationFile;
+    private ApplicationData applicationData;
 
     private void Start()
     {
+        pathToApplicationFile = Application.persistentDataPath + $"/ApplicationData.dap";
+        saveManagerIO = new ReservationManagerIO(pathToApplicationFile);
+        applicationData = saveManagerIO.LoadReservationApplicationData();
+
+        if (applicationData != null)
+        {
+            if (IsThereSave())
+            {
+                buttonContinue.gameObject.SetActive(true);
+                buttonNewGame.gameObject.SetActive(true);
+                buttonBegin.gameObject.SetActive(false);
+            }
+            else
+            {
+                buttonContinue.gameObject.SetActive(false);
+                buttonNewGame.gameObject.SetActive(false);
+                buttonBegin.gameObject.SetActive(true);
+            }
+        }
+
         Time.timeScale = 1;
         fade.FadeWhite();
     }
@@ -35,9 +63,37 @@ public class MenuUIController : MonoBehaviour
         }
     }
 
+    public void NewGame()
+    {
+        saveManagerIO = new ReservationManagerIO(Application.persistentDataPath);
+        saveManagerIO.DeleteAll();
+
+        LoadLevel(2);
+    }
+
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public bool IsThereSave()
+    {
+        pathToApplicationFile = Application.persistentDataPath + $"/ApplicationData.dap";
+        saveManagerIO = new ReservationManagerIO(pathToApplicationFile);
+        applicationData = saveManagerIO.LoadReservationApplicationData();
+
+        if (applicationData != null)
+        {
+            for (int i = 0; i < applicationData.reservationElementUIData.Count; i++)
+            {
+                if (applicationData.reservationElementUIData[i] != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        return false;
     }
 
     public void ApllicationQuit()
