cb21836a57836e3ba7769e58bd5ff1dd1a6b7552
Денис <94766316+DSD-Games@users.noreply.github.com>
2023-08-30 14:21:18 +0700

    0.61
    - Сделал анимацию косноли

---
+++ Assets/Scripts/Console/ConsoleMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ConsoleMessage
+{
+    [Header("Ñêîðîñòü òåêñòà â äèàëîãè")]
+    public float speedText;
+    [Header("Íàñòðîéêà òåêñòà äèàëîãà")]
+    public Color colorText = Color.white;
+    public Font fontText;
+    public FontStyle fontStyleText;
+    [Header("Ïðåäëîæåíèÿ è îòâåòû")]
+    [TextArea(3, 10)]
+    public string Message;
+    [Header("Ñîáûòèÿ êîíöà äèàëîãà")]
+    public UnityEvent EndWriteMessage;
+    [Header("Îæèäàíèå äèàëîãà")]
+    public float waitSecond;
+    [Header("Анимация написания текста")]
+    public bool animatedWriteText = false;
+    [Header("Очищать текстовое поле")]
+    public bool clearTextField = false;
+}
